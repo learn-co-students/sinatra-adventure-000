5bf222d5f1c3ef8bb4f8a58ca8ec5cfd9a3ae42a
ahimmelstoss <ahimmelstoss@gmail.com>
2014-10-22 16:29:26 -0400

    bunlde update, readme update, added sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < Sinatra::Base
 
   set :views, File.expand_path('../../views', __FILE__)
 
+  set :sessions, true
+
   configure :development do
     register Sinatra::Reloader
   end
