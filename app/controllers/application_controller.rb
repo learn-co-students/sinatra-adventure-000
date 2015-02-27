4ac8df651c93d2551498c01b31aa0a50c5570f31
Ian Miller <irmiller22@gmail.com>
2015-02-27 10:31:59 -0500

    fixed session issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < Sinatra::Base
   set :views, File.expand_path('../../views', __FILE__)
 
   set :sessions, true
+  set :session_secret, "My session secret"
 
   configure :development do
     register Sinatra::Reloader
