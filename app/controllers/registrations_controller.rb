ddf81808f56490f5ed5051caa4634794b097504f
garettarrowood <garettarrowood@gmail.com>
2015-08-24 15:04:27 -0400

    need help, where did everyone go?

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,16 +10,15 @@ class RegistrationsController < ApplicationController
   # appropriate route, look at the request, and then
   # direct them to the new user page via the /new route.
 
-  # TODO: Create a route that handles a POST HTTP request from the
-  # registration form, then see what /new has to say.
+  ##### TODO: Create a route that handles a POST HTTP request from the
+  ##### registration form, then see what /new has to say.
 
   # Note: you aren't creating persistence for a user. This
   # is a very dumb, single-request registration process.
   # It is the wise adventurer that doesn't overthink the task!
 
-
   get '/new' do
-    throw Unauthorized unless user_registered?
+    redirect '/register' unless user_registered?
     # keep this line of code in place to protect this sacred
     # page from interlopers who have not properly completed
     # the maze you are constructing. You will need to implement
@@ -30,7 +29,7 @@ class RegistrationsController < ApplicationController
     # You will need to send properly registered people to me
     # and render a template that will tell them they
     # have completed their quest
-
+    erb :new_user
     # TODO: render the new user template and see what it says.
   end
   # Bonus experience! The throw above makes an ugly error page happen.
@@ -39,9 +38,11 @@ class RegistrationsController < ApplicationController
   # them what they've done wrong?
 
   def user_registered?
-    # TODO: you'll need a way for your registration to set a value that
-    # will make this true when your /new looks at it.
-    false
+    session[:info] = [params[:name], params[:email]]
+    if session[:info] == nil
+      return false
+    end
+    true
   end
 
 end
