90a9e96424f77966263a1a076412565d461311bb
NoneOfMaster <rcraigt@gmail.com>
2015-12-28 13:57:12 -0800

    Capybara won't click

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -13,6 +13,22 @@ class RegistrationsController < ApplicationController
   # TODO: Create a route that handles a POST HTTP request from the
   # registration form, then see what /new has to say.
 
+  enable :sessions
+
+  helpers do 
+
+    def user_registered?
+      !!session[:email] && !!session[:password]
+    end
+
+  end
+
+  post "/handler" do
+    session[:email] = params[:email_address]
+    session[:password] = params[:password]
+    redirect "/new"
+  end
+
   # Note: you aren't creating persistence for a user. This
   # is a very dumb, single-request registration process.
   # It is the wise adventurer that doesn't overthink the task!
@@ -32,16 +48,12 @@ class RegistrationsController < ApplicationController
     # have completed their quest
 
     # TODO: render the new user template and see what it says.
+
+    erb :'/new_user'
   end
   # Bonus experience! The throw above makes an ugly error page happen.
   # Can you check for the same condition, but send them back to
   # the registration page with an error message that will tell
   # them what they've done wrong?
-
-  def user_registered?
-    # TODO: you'll need a way for your registration to set a value that
-    # will make this true when your /new looks at it.
-    false
-  end
 
 end
